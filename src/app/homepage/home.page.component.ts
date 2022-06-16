7e827cfa1883588367050089e67af9bf3d96414d
artsmc <artsmc88@gmail.com>
2022-06-16 10:33:49 -0400

    9 files modified, 1 added, 1 deleted

--- src/app/homepage/home.page.component.ts
+++ src/app/homepage/home.page.component.ts
@@ -1,11 +1,15 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
 import * as anime from 'animejs';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-render-homepage',
   templateUrl: './home.page.component.html',
   styleUrls: ['./home.page.component.scss'],
 })
-export class HomepageComponent {
-  name = 'Angular ' + VERSION.major;
+export class HomepageComponent implements OnInit, OnDestroy {
+  ngOnInit(): void {
+    AOS.init();
+  }
+  ngOnDestroy(): void {}
 }
